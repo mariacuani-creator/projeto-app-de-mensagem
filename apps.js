837e12a31902c996bdcd04c3403c6936bf616bf2
mariacuani-creator <maria.cuani@escola.pr.gov.br>
2026-04-14 10:14:23 -0300

    Add files via upload

--- apps.js
+++ apps.js
@@ -2,17 +2,15 @@
 //cria array 0 1 2
 
 let frases = ["F1", "F2", "F3"];
-let frasesCopiada = [...frases];
-//gera as frases
-function geradorMensagem() {
-    if (frases.length == 0) {
-        frases = [...frasesCopiada];
-    }
-    let indiceGerado = Math.floor(Math.random() * (frases.length));
-    let fraseGerada = frases[indiceGerado];
-
-    document.querySelector("#msg").textContent = fraseGerada;
-   
-
+let frasesCopiada = [...frase];
+function geradorMensagem(){
+  if(frases.length==0){
+    frases=[...frasesCopiada];
+  }
+  let indiceGerado = Math.floor(Math.random()*(frases.length));
+  let fraseGerada = frases[indiceGerado];
+  console.log(fraseGerada)
+  document.querySelector("#msg").textContent=fraseGerada;
+  frases.splice(indiceGerado,1);
 }
-geradorMensagem();
+geradorMensagem()
